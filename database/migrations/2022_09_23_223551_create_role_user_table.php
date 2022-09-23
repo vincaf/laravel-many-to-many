d6cc799651aa9e6caeffaf42cc41a5949e94cb2b
vincaf <vincenzocafassog@gmail.com>
2022-09-24 01:34:22 +0200

    feat(CreateRoleUser): add primary key

--- database/migrations/2022_09_23_223551_create_role_user_table.php
+++ database/migrations/2022_09_23_223551_create_role_user_table.php
@@ -21,6 +21,8 @@ class CreateRoleUserTable extends Migration
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
+            $table->primary(['role_id', 'user_id']);
+
             $table->timestamps();
         });
     }
