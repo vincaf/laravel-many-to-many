6d6295fda503318d0559efcff9e6aa26d65ee6a5
vincaf <vincenzocafassog@gmail.com>
2022-09-24 03:10:43 +0200

    feat(Models): add relation many2many posts-tags

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -23,4 +23,8 @@ class Post extends Model
     public function user(){
         return $this->belongsTo('App\User');
     }
+
+    public function tags(){
+        return $this->belongsToMany('App\Models\Tag');
+    }
 }
