136273adae93ee2e4c8216d324e2ac2d27743970
vincaf <vincenzocafassog@gmail.com>
2022-09-24 02:17:08 +0200

    feat: create Role User seeder

---
+++ database/seeds/RoleUserTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\Role;
+use App\User;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class RoleUserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $roles = Role::all();
+        $users = User::all();
+
+        foreach ($users as $user) {
+            $randomRoles = $faker->randomElements($roles, 2, false);
+
+            foreach ($randomRoles as $randomRole) {
+                $user->roles()->attach($randomRole->id);
+            }
+        }
+    }
+}
