8731b8938d6f2f4c325a03e2e39ce2df6b2226f9
vincaf <vincenzocafassog@gmail.com>
2022-09-24 03:06:45 +0200

    feat(TagsSeeder): create seeder for tags

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Tag;
+use Illuminate\Database\Seeder;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = [
+            '#movie',
+            '#series',
+            '#cinema',
+            '#films',
+            '#hollywood',
+            '#serieA',
+            'netflix',
+            'primevideo',
+            'dazn',
+        ];
+
+        foreach ($tags as $tag){
+            $newTag = new Tag();
+            $newTag->name = $tag;
+            $newTag->save();
+        }
+    }
+}
